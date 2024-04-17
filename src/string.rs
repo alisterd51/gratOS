88bfebfcf09f33fafa13a15f7d7c7e1e22d26fdb
Antoine CLARMAN <antoinereims28@gmail.com>
2024-04-17 22:33:36 +0200

    feat(string): add memcpy, memset and memcmp func

---
+++ src/string.rs
@@ -0,0 +1,38 @@
+use core::ffi::{c_char, c_int, c_size_t, c_void};
+
+#[no_mangle]
+pub unsafe extern "C" fn memcpy(dest: *mut c_void, src: *const c_void, n: c_size_t) -> *mut c_void {
+    let mut i: c_size_t = 0;
+
+    while i < n {
+        *(dest as *mut c_char).wrapping_add(i) = *(src as *const c_char).wrapping_add(i);
+        i += 1;
+    }
+    dest
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn memset(dest: *mut c_void, c: c_int, n: c_size_t) -> *mut c_void {
+    let mut i: c_size_t = 0;
+
+    while i < n {
+        *(dest as *mut c_char).wrapping_add(i) = c as c_char;
+        i += 1;
+    }
+    dest
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn memcmp(s1: *const c_void, s2: *const c_void, n: c_size_t) -> c_int {
+    let mut i: c_size_t = 0;
+
+    while i < n {
+        let diff = *(s1 as *const c_char).wrapping_add(i) - *(s2 as *const c_char).wrapping_add(i);
+
+        if diff != 0 {
+            return diff as c_int;
+        }
+        i += 1;
+    }
+    0
+}
