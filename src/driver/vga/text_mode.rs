9577391e6dfff07c196817de00b193e92b58817f
Antoine Clarman <antoinereims28@gmail.com>
2024-05-21 14:56:45 +0200

    feat(driver-vga-text_mode): add history

--- src/driver/vga/text_mode.rs
+++ src/driver/vga/text_mode.rs
@@ -1,48 +1,9 @@
+use super::{
+    history_buffer::HistoryBuffer, Color, ColorCode, ScreenChar, BUFFER_HEIGHT, BUFFER_WIDTH,
+};
 use crate::io::outb;
 use core::fmt;
 use spin::{Lazy, Mutex};
-
-#[allow(dead_code)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u8)]
-pub enum Color {
-    Black = 0,
-    Blue = 1,
-    Green = 2,
-    Cyan = 3,
-    Red = 4,
-    Magenta = 5,
-    Brown = 6,
-    LightGray = 7,
-    DarkGray = 8,
-    LightBlue = 9,
-    LightGreen = 10,
-    LightCyan = 11,
-    LightRed = 12,
-    Pink = 13,
-    Yellow = 14,
-    White = 15,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(transparent)]
-struct ColorCode(u8);
-
-impl ColorCode {
-    const fn new(foreground: Color, background: Color) -> ColorCode {
-        ColorCode((background as u8) << 4 | (foreground as u8))
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(C)]
-struct ScreenChar {
-    ascii_character: u8,
-    color_code: ColorCode,
-}
-
-const BUFFER_HEIGHT: usize = 25;
-const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
 struct Buffer {
@@ -54,6 +15,7 @@ pub struct Writer {
     column_position: usize,
     color_code: ColorCode,
     buffer: &'static mut Buffer,
+    history: HistoryBuffer,
 }
 
 impl Writer {
@@ -96,11 +58,13 @@ impl Writer {
         let row = self.row_position;
         let col = self.column_position;
         let color_code = self.color_code;
-
-        self.buffer.chars[row][col] = ScreenChar {
+        let c = ScreenChar {
             ascii_character: byte,
             color_code,
         };
+
+        self.buffer.chars[row][col] = c;
+        self.history.set_char(col, row, c);
     }
 
     fn new_line(&mut self) {
@@ -124,23 +88,30 @@ impl Writer {
     }
 
     fn next_line(&mut self) {
-        for row in 1..BUFFER_HEIGHT {
-            for col in 0..BUFFER_WIDTH {
-                let character = self.buffer.chars[row][col];
-                self.buffer.chars[row - 1][col] = character;
+        if self.history.next_line().is_ok() {
+            for row in 0..BUFFER_HEIGHT {
+                if let Ok(line) = self.history.get_line(row) {
+                    self.buffer.chars[row] = line;
+                }
+            }
+        } else {
+            self.history.new_line();
+            for row in 0..BUFFER_HEIGHT {
+                if let Ok(line) = self.history.get_line(row) {
+                    self.buffer.chars[row] = line;
+                }
             }
         }
-        self.clear_row(BUFFER_HEIGHT - 1);
     }
 
     fn previous_line(&mut self) {
-        for row in (0..BUFFER_HEIGHT - 1).rev() {
-            for col in 0..BUFFER_WIDTH {
-                let character = self.buffer.chars[row][col];
-                self.buffer.chars[row + 1][col] = character;
+        if self.history.previous_line().is_ok() {
+            for row in 0..BUFFER_HEIGHT {
+                if let Ok(line) = self.history.get_line(row) {
+                    self.buffer.chars[row] = line;
+                }
             }
         }
-        self.clear_row(0);
     }
 
     fn clear_row(&mut self, row: usize) {
@@ -150,6 +121,7 @@ impl Writer {
         };
         for col in 0..BUFFER_WIDTH {
             self.buffer.chars[row][col] = blank;
+            self.history.set_char(col, row, blank);
         }
     }
 
@@ -246,6 +218,7 @@ static WRITER: Lazy<Mutex<Writer>> = Lazy::new(|| {
         column_position: 0,
         color_code: ColorCode::new(Color::LightGray, Color::Black),
         buffer: unsafe { &mut *(0xB8000 as *mut Buffer) },
+        history: HistoryBuffer::new(),
     })
 });
 
