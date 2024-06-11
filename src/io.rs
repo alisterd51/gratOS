afec3e2901321daa259b1c7aa34a268982fc8427
Antoine Clarman <antoinereims28@gmail.com>
2024-06-11 12:19:05 +0200

    chore: prepare edition2024

--- src/io.rs
+++ src/io.rs
@@ -5,19 +5,23 @@ use core::arch::asm;
 pub unsafe fn inb(port: u16) -> u8 {
     let value;
 
-    asm!(
-        "in al, dx",
-        out("al") value,
-        in("dx") port
-    );
+    unsafe {
+        asm!(
+            "in al, dx",
+            out("al") value,
+            in("dx") port
+        );
+    }
     value
 }
 
 /// Write a one byte data to port
 pub unsafe fn outb(value: u8, port: u16) {
-    asm!(
-        "out dx, al",
-        in("al") value,
-        in("dx") port
-    );
+    unsafe {
+        asm!(
+            "out dx, al",
+            in("al") value,
+            in("dx") port
+        );
+    }
 }
